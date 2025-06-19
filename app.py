44bb874224e24fa3cfdfa754e29835f4f92348a3
Benny Eisner <b.a.eisner@wustl.edu>
2025-06-18 23:31:23 -0400

    feat(api): add start_date, end_date params for fetch_data

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import FastAPI, HTTPException
 
 from fetch_data import get_price_data
@@ -5,11 +7,11 @@ from fetch_data import get_price_data
 app = FastAPI(title="Simple yfinance API")
 
 
-@app.get("/prices/{ticker}")
-def read_prices(ticker: str):
+@app.get("/prices/{ticker}/{start_date}/{end_date}")
+def read_prices(ticker: str, start_date: date, end_date: date):
     """HTTP GET returns history of ticker data as JSON"""
     try:
-        df = get_price_data(ticker)
+        df = get_price_data(ticker, start_date, end_date)
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
 
--- fetch_data.py
+++ fetch_data.py
@@ -1,12 +1,16 @@
+from datetime import date
+
 import yfinance as yf
 
 
-def get_price_data(ticker: str):
-    stock = yf.Ticker(ticker)
-    df = stock.history(period="5d")
+def get_price_data(ticker: str, start: date, end: date):
+    df = yf.Ticker(ticker).history(start=start.isoformat(), end=end.isoformat())
     return df
 
 
+def main():
+    pass
+
+
 if __name__ == "__main__":
-    sample = get_price_data("AAPL")
-    print(sample)
+    main()
